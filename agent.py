0025b1633308146c4455fb6444b908a78c9ce3d5
ArnavM3434 <arnavnmehta1@gmail.com>
2024-05-28 19:21:40 -0500

    s

---
+++ agent.py
@@ -0,0 +1,20 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+
+#play around with these parameters
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        pass
+
+    def get_state(self, game):
+        pass
+
+    def remember(self,)
